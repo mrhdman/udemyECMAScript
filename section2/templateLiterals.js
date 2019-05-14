a1face2666e60aeb37e12033cf0464c71a4636a9
mrhdman <mattrhavins@gmail.com>
2019-05-14 13:02:46 -0400

    template literals

---
+++ section2/templateLiterals.js
@@ -0,0 +1,20 @@
+const name = "Matt";
+
+const hello = `Hello, ${name}`;
+console.log(hello);
+
+const string = `It was a rainy day.
+It was also a cold day.
+It was a relaxing day.`
+console.log(string);
+
+const anExpression = `7 * 13 = ${7*13}`;
+console.log(anExpression);
+
+let isMember = true;
+const aTernary = `Your price is ${isMember ? "$7.00" : "$14.00"}.`
+console.log(aTernary);
+
+isMember = false;
+const aTernary2 = `Your price is ${isMember ? "$7.00" : "$14.00"}.`
+console.log(aTernary2);
